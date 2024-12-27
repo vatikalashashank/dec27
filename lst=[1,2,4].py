7b5795990a49c193bca3a44447c84d25f4f7dd85
vatikalashashank <vatikalashashank@gmail.com>
2024-12-27 08:57:04 +0530

    Create lst=[1,2,4].py

---
+++ lst=[1,2,4].py
@@ -0,0 +1,16 @@
+# lst=[1,2,4]
+# lst.append(20)
+# print(lst)
+
+# sales_2023=[10,20]
+# sales_2024=[1,44]
+# sales_2023.append(sales_2024)
+# print(sales_2023)
+
+# sales_2023=[10,50]
+# sales_2023.insert(1,'rama')
+# print(sales_2023)
+
+# val=[56468,7766,644,94156]
+# val.sort(reverse=True)
+# print(val)
